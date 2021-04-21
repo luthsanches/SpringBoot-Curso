15854ce3b410694f4309cd8cc48ed346f3afddd3
Luth <luthsanches@gmail.com>
2021-04-21 09:16:26 -0300

    commit

---
+++ curso-spring-boot/src/main/java/io/github/dougllasfps/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,24 @@
+package io.github.dougllasfps.rest.controller;
+
+import io.github.dougllasfps.exception.PedidoNaoEncontradoException;
+import io.github.dougllasfps.exception.RegraNegocioException;
+import io.github.dougllasfps.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex){
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+
+    @ExceptionHandler(PedidoNaoEncontradoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiErrors handlePedidoNotFoundException( PedidoNaoEncontradoException ex ){
+        return new ApiErrors(ex.getMessage());
+    }
+}
